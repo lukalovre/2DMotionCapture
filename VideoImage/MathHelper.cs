e5654df57f794f94b416fd07befbd155d6a722e6
lukalovre <lukalovre92@gmail.com>
2018-11-05 23:00:06 +0100

    Revert "r"
    This reverts commit 4c9af866cebcf9248ead25f486236eb1e251d1db.

--- VideoImage/MathHelper.cs
+++ VideoImage/MathHelper.cs
@@ -15,7 +15,7 @@ namespace MoCap
 
         public static float Distance(PointF a, PointF b)
         {
-            return (float)Math.Sqrt(Math.Pow(a.X - b.X, 2 ) + Math.Pow(a.Y - b.Y, 2));
+            return (float)Math.Sqrt(Math.Pow(a.X - b.X, 2) + Math.Pow(a.Y - b.Y, 2));
         }
 
         public static PointF Add(PointF a, PointF b)
